1d2f1ccca0f739391d898cda81bd39acef990239
JhonTabio <JhonTabioCS@gmail.com>
2025-11-15 23:10:00 -0500

    feat(core) create basic Swing window
    Static size and border layout

--- src/main/java/com/jhontabio/inspector/TreeTap.java
+++ src/main/java/com/jhontabio/inspector/TreeTap.java
@@ -1,9 +1,33 @@
 package com.jhontabio.inspector;
 
-public class TreeTap
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+public class TreeTap extends JFrame
 {
+	public TreeTap()
+	{
+		super("TreeTap - Brigadier Inspector");
+
+		// Configuration
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize(900, 900);
+		setLocationByPlatform(true);
+		setLayout(new BorderLayout());
+
+		// UI Layout
+		add(new JLabel("Hello Swing!", SwingConstants.CENTER), BorderLayout.CENTER);
+	}
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
+
+		TreeTap tt = new TreeTap();
+		tt.setVisible(true);
 	}
 }
