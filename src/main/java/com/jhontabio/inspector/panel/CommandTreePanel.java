a75325413dffa06800160f474050eb0742362361
Jhon Tabio <JhonTabioCS@gmail.com>
2025-11-20 20:47:51 -0500

    feat(CommandTreePanel): add MouseListener events for mouse click
    Just added debug text that gets printed on click for now

--- src/main/java/com/jhontabio/inspector/panel/CommandTreePanel.java
+++ src/main/java/com/jhontabio/inspector/panel/CommandTreePanel.java
@@ -3,13 +3,24 @@ package com.jhontabio.inspector.panel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JPanel;
 
 public class CommandTreePanel extends JPanel
 {
 	public CommandTreePanel()
 	{
-		super(new BorderLayout());
+		super(null);
+
+		addMouseListener(new MouseAdapter()
+		{
+			public void mousePressed(MouseEvent e)
+			{
+				System.out.println("Mouse Pressed!");
+			}
+		});
 
 		Vertex v = new Vertex("Test");
 		v.setBackground(Color.DARK_GRAY);
