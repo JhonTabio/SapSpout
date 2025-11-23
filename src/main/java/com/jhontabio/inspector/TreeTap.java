f901be5e27a7f26042769e24b612c7c37a07c70b
Jhon Tabio <JhonTabioCS@gmail.com>
2025-11-23 14:21:38 -0500

    feat(core): cetner tree panel on startup
    cleaned up comments / old code
    
    Set window pop up position to the center

--- src/main/java/com/jhontabio/inspector/TreeTap.java
+++ src/main/java/com/jhontabio/inspector/TreeTap.java
@@ -2,12 +2,14 @@ package com.jhontabio.inspector;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JViewport;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
@@ -18,6 +20,7 @@ import com.jhontabio.inspector.panel.CommandTreePanel;
 public class TreeTap extends JFrame
 {
 	private final CommandTreePanel ctp;
+	private final JScrollPane scrollPane;
 
 	public TreeTap()
 	{
@@ -31,17 +34,16 @@ public class TreeTap extends JFrame
 
 		// Tree visualization
 		ctp = new CommandTreePanel();
-		//ctp.setPreferredSize(new Dimension(ctp.getWidth() + 1000, ctp.getHeight() + 1000));
-		JScrollPane scroll = new JScrollPane(ctp);
-		//scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		//scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane = new JScrollPane(ctp);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 
 		// Command input
 		JPanel testPanel = new JPanel();
 		testPanel.add(new JLabel("Hello Swing!", SwingConstants.CENTER), BorderLayout.CENTER);
 
 		// Divide panels
-		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, testPanel);
+		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, testPanel);
 		split.setDividerLocation(350);
 		split.setResizeWeight(0.3);
 
@@ -56,6 +58,22 @@ public class TreeTap extends JFrame
 		SwingUtilities.invokeLater(() -> {
 			TreeTap tt = new TreeTap();
 			tt.setVisible(true);
+			tt.setLocationRelativeTo(null);
+
+			SwingUtilities.invokeLater(() -> {
+				JViewport viewport = tt.scrollPane.getViewport();
+
+				Dimension viewSize = tt.ctp.getPreferredSize();
+				Dimension extentSize = viewport.getExtentSize();
+
+				int x = (viewSize.width - extentSize.width) / 2;
+				int y = (viewSize.height - extentSize.height) / 2;
+
+				if(x < 0) x = 0;
+				if(y < 0) y = 0;
+
+				viewport.setViewPosition(new Point(x, y));
+			});
 		});
 	}
 }
